81deba3c74023ab34865b4e64f6640302b1a515c
Alan Sartorio <alan42ga@hotmail.com>
2023-08-10 17:43:39 -0300

    Add plan scraping

--- .github/scrape.sh
+++ .github/scrape.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-mkdir -p data
-touch data/test-file.json
+mkdir -p "data/plans"
+mkdir -p "data/commissions"
+
+while read plan
+do
+	echo "PLAN: $plan"
+	url=$(PLAN=$plan envsubst <<< $PLAN_URL)
+	wget "$url" -O - | jq . > "data/plans/$plan"
+done < .github/plans.txt
