4128730020a1bc0befa0c44e884dd0e46557d540
Alan Sartorio <alan42ga@hotmail.com>
2023-08-10 19:29:09 -0300

    Complete plans list, scrape commissions

--- .github/scrape.sh
+++ .github/scrape.sh
@@ -3,9 +3,66 @@
 mkdir -p "data/plans"
 mkdir -p "data/commissions"
 
-while read plan
-do
-	echo "PLAN: $plan"
-	url=$(PLAN=$plan envsubst <<< $PLAN_URL)
-	wget "$url" -O - | jq . > "data/plans/$plan"
-done < .github/plans.txt
+function getJson() {
+	wget "$1" -O - | jq .
+}
+
+function scrapePlans() {
+	jq -r 'flatten(1) | .[]' .github/plans.json | while read plan
+	do
+		urlEncoded=$(echo $plan | jq -sRr @uri)
+		echo "PLAN: $plan"
+		url=$(PLAN="$urlEncoded" envsubst <<< $PLAN_URL)
+		getJson "$url" > "data/plans/$plan.json"
+	done
+}
+
+function scrapeCommissionsBase() {
+	level="GRADUATE"
+	year="$1"
+	period="$2"
+	url=$(LEVEL="$level" YEAR="$year" PERIOD="$period" envsubst <<< $COMMISSIONS_URL)
+	echo year: $1
+	echo period: $2
+	echo $url
+	getJson "$url" > "data/commissions/$level-$year-$period.json"
+}
+
+function getNextSemester() {
+	year=$1
+	semester=$2
+	if [ "$semester" = "FirstSemester" ]
+	then
+		echo $year "SecondSemester"
+	else
+		echo $((year+1)) "FirstSemester"
+	fi
+}
+
+function getSemesterFromMonth() {
+	year=$1
+	month=$2
+	case $month in
+		01 | 02 | 03 | 04 | 05 | 06)
+			echo $year "FirstSemester"
+			;;
+		07 | 08 | 09 | 10 | 11)
+			echo $year "SecondSemester"
+			;;
+		12)
+			echo $((year+1)) "FirstSemester"
+			;;
+	esac
+}
+
+function scrapeCommissions() {
+	read -r year month <<<$(date +"%Y %m")
+	read -r year semester <<<$(getSemesterFromMonth $year $month)
+
+	scrapeCommissionsBase $year $semester
+	read -r nextYear nextSemester <<<$(getNextSemester $year $semester)
+	scrapeCommissionsBase $nextYear $nextSemester
+}
+
+scrapePlans
+scrapeCommissions
